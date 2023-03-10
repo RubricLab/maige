774ef309697b6062b0415d411d5dd3cdb1fc8408
tedspare <ted.spare@gmail.com>
2023-03-10 13:49:05 -0500

    Remove required env vars

---
+++ src/pages/api/webhook.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next/types";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    return res.status(405).send({
+      success: false,
+      message: "Only POST requests are accepted.",
+    });
+  }
+
+  const { data } = req.body;
+
+  console.log("Webhook received! Data", data);
+
+  return res.status(200).send({
+    success: true,
+    message: "Webhook received",
+  });
+};
