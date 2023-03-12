a64a7871af5c25969fe616c7e14313debba38950
tedspare <ted.spare@gmail.com>
2023-03-12 18:52:56 -0400

    Add waitlist

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useCallback, useState } from "react";
 import wizardHatLogo from "../../public/logo.png";
 
 const title = "Maige";
@@ -9,6 +10,28 @@ const description = "Let GPT label your issues.";
 const linkPreview = "/logo.png";
 
 const Home: NextPage = () => {
+  const [email, setEmail] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const submitEmail = useCallback(async () => {
+    if (!email || isSubmitting) return;
+
+    setIsSubmitting(true);
+
+    await fetch("/api/email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+    });
+
+    setIsSubmitting(false);
+    setEmail("");
+    setIsSubmitted(true);
+  }, [email]);
+
   return (
     <div className="bg-black">
       <Head>
@@ -70,12 +93,34 @@ const Home: NextPage = () => {
           <h2 className="text-3xl font-medium tracking-tight text-white/70">
             Let GPT label your issues.
           </h2>
-          <p className="pb-8 italic text-white/60">Magically.</p>
-          <Link href="https://github.com/apps/maige-bot">
+          <p className="pb-8 italic text-white/60">Maigically.</p>
+          {isSubmitted ? (
+            <p className="text-green-600">Thanks! We&apos;ll be in touch.</p>
+          ) : (
+            <p className="text-white/80">Join the waitlist:</p>
+          )}
+          <div className="w-56 space-y-4">
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full rounded-md border border-gray-700 bg-gray-900 p-2 px-3 text-white transition-colors hover:border-gray-600 focus:outline-none focus:ring-4 focus:ring-green-600/60 active:border-gray-600"
+              placeholder="me@startup.so"
+              disabled={isSubmitted || isSubmitting}
+            />
+            <button
+              onClick={submitEmail}
+              disabled={!email || isSubmitted || isSubmitting}
+              className="w-full rounded-md border-gray-900 bg-green-700 p-2 font-medium text-white transition-all enabled:hover:bg-green-600 disabled:opacity-80"
+            >
+              Submit
+            </button>
+          </div>
+          {/* <Link href="https://github.com/apps/maige-bot">
             <button className="rounded-md border-gray-900 bg-green-700 px-6 py-3 font-medium text-white blur-sm transition-all duration-500 hover:scale-105 hover:bg-green-600 hover:blur-none">
               Install with GitHub
             </button>
-          </Link>
+          </Link> */}
         </div>
       </main>
       <footer className="absolute bottom-0 flex h-10 w-screen items-center justify-center">
