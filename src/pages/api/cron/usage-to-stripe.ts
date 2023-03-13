c0540c63d8d6ed8e23840d72fe0089851eeeaa71
tedspare <ted.spare@gmail.com>
2023-03-12 21:52:19 -0400

    Add Cron job to sync usage to Stripe daily

---
+++ src/pages/api/cron/usage-to-stripe.ts
@@ -0,0 +1,53 @@
+import type { NextApiRequest, NextApiResponse } from "next/types";
+import prisma from "~/lib/prisma";
+import { stripe } from "~/lib/stripe";
+
+// TODO: import this from .env in Vercel.json. Hardcoded for now.
+const CRON_KEY = "9D7042C6-CEE2-454A-8E4F-65BD8976DA7F";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    return res.status(405).send({
+      message: "Only POST requests are accepted.",
+    });
+  }
+
+  if (req.query.key !== CRON_KEY) {
+    return res.status(403).send({
+      message: "Bad cron key.",
+    });
+  }
+
+  const now = new Date().getTime();
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  const activeCustomers = await prisma.customer.findMany({
+    where: {
+      usageUpdatedAt: {
+        gte: new Date(now - oneDay),
+      },
+    },
+  });
+
+  for (const customer of activeCustomers) {
+    const { stripeCustomerId, usage } = customer;
+
+    if (!stripeCustomerId) {
+      // TODO: send email to trial user if usage is over 10
+      continue;
+    }
+
+    await stripe.subscriptionItems.createUsageRecord(
+      "si_NVzE7g7DQ9hzEQ", // hardcoded to my subscription
+      {
+        quantity: usage,
+        timestamp: Math.floor(new Date().getTime() / 1000),
+        action: "set",
+      }
+    );
+  }
+
+  return res.status(200).send({
+    message: "Cron request received",
+  });
+};
