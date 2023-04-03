4713f168705aef13f00fcd92860e9fa731d92283
tedspare <ted.spare@gmail.com>
2023-04-02 21:47:26 -0400

    Remove NextAuth for now

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,8 @@
 import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-
 import "~/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
