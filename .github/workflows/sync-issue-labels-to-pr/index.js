53c6319f39c8c35b087bbb968611b1354996f01f
tedspare <ted.spare@gmail.com>
2023-04-03 10:07:34 -0400

    Fix referenced issue fetch

--- .github/workflows/sync-issue-labels-to-pr/index.js
+++ .github/workflows/sync-issue-labels-to-pr/index.js
@@ -3,19 +3,38 @@
 const { Octokit } = require("@octokit/core");
 
 async function applyLabelFromLinkedIssueToPR(pr, octokit) {
+  const timelineItems = await octokit.request(
+    "GET /repos/{owner}/{repo}/issues/{issue_number}/timeline",
+    {
+      owner: pr.base.repo.owner.login,
+      repo: pr.base.repo.name,
+      issue_number: pr.number,
+      mediaType: {
+        previews: ["mockingbird"],
+      },
+    }
+  );
+
+  const linkedIssues = timelineItems.data.filter(
+    (item) =>
+      item.event === "cross-referenced" && item.source && item.source.issue
+  );
+
+  if (linkedIssues.length === 0) {
+    console.log("No issue linked.");
+    return;
+  }
+
+  // Fetch the first linked issue
+  const issueNumber = linkedIssues[0].source.issue.number;
   const linkedIssue = await octokit.request(
     "GET /repos/{owner}/{repo}/issues/{issue_number}",
     {
       owner: pr.base.repo.owner.login,
       repo: pr.base.repo.name,
-      issue_number: pr.head.ref,
+      issue_number: issueNumber,
     }
   );
-
-  if (!linkedIssue) {
-    console.log("No issue linked.");
-    return;
-  }
 
   const linkedIssueLabels = linkedIssue.labels.map((label) => label.name);
 
