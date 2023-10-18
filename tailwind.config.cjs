93a4fa316710416082a09c9cd10a13af6aa3597b
tedspare <ted.spare@gmail.com>
2023-10-18 14:36:47 -0400

    Update Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
