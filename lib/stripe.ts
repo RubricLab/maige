dc22e46edef808127d63325e326ea2074b194f97
DexterStorey <dexter@dexterstorey.com>
2023-11-17 14:52:14 -0500

    stripe bump

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-	apiVersion: '2022-11-15',
+	apiVersion: '2023-10-16',
 	appInfo: {
 		name: 'Maige',
 		version: '0.1.0'
