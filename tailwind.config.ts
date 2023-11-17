525ca4cfe8cb481d11710919a3f0e9ddf4fb2680
DexterStorey <dexter@dexterstorey.com>
2023-11-17 14:26:21 -0500

    rubric tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import config from '@rubriclab/tailwind-config'
+import {Config} from 'tailwindcss'
+
+const tailwindConfig = {
+	content: ['./app/**/*.tsx', './lib/**/*.tsx'],
+	presets: [config],
+} satisfies Config
+
+export default tailwindConfig
