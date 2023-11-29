48df5f7b6561b5be6504b6f3405431298be31279
arihanv <63890951+arihanv@users.noreply.github.com>
2023-11-29 09:38:28 -0600

    Specify Weaviate schema (#49)
    * Specify Weaviate schema in code

---
+++ lib/utils/embeddings/setup/index.ts
@@ -0,0 +1,21 @@
+import weaviate from 'weaviate-ts-client'
+import {schema} from './schema'
+
+export async function main() {
+	console.log('Grabbing Weaviate schema template...')
+
+	console.log('Creating Weaviate schema...')
+	const client = weaviate.client({
+		scheme: 'https',
+		host: process.env.WEAVIATE_HOST
+	})
+
+	try {
+		await client.schema.classCreator().withClass(schema).do()
+		console.log('Schema created!')
+	} catch (e: unknown) {
+		console.log(e instanceof Error && e.message)
+	}
+}
+
+main()
