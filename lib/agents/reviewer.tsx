836883ebbb17ee2cbeaadcf6d6073db2c649a839
arihanv <arihanvaranasi@gmail.com>
2023-12-05 20:53:34 -0600

    feat: reviewer can use prComment & codeComment

--- lib/agents/reviewer.tsx
+++ lib/agents/reviewer.tsx
@@ -1,9 +1,13 @@
+import {OctokitReqType} from '@octokit/app'
 import {initializeAgentExecutorWithOptions} from 'langchain/agents'
 import {ChatOpenAI} from 'langchain/chat_models/openai'
 import {SerpAPI} from 'langchain/tools'
 import env from '~/env.mjs'
+import {codeComment} from '~/tools/codeComment'
 import {prComment} from '~/tools/prComment'
 import {isDev} from '~/utils'
+
+var parse = require('parse-diff')
 
 const model = new ChatOpenAI({
 	modelName: 'gpt-4-1106-preview',
@@ -14,34 +18,89 @@ const model = new ChatOpenAI({
 export default async function reviewer({
 	input,
 	octokit,
-	pullId
+	pullId,
+	owner,
+	repo,
+	pull_number,
+	head
 }: {
 	input: string
-	octokit: any
-	pullId: string
+	octokit: OctokitReqType
+	pullId?: string
+	owner?: string
+	repo?: string
+	pull_number?: number
+	head?: string
 }) {
-	const tools = [new SerpAPI(), prComment({octokit, pullId})]
+	if (pullId) {
+		const tools = [new SerpAPI(), prComment({octokit, pullId})]
 
-	const prefix = `
-	You are senior engineer reviewing a Pull Request in GitHub made by a junior engineer.
-		You MUST leave a comment on the PR according to the user's instructions using the prComment function.
-    Format your answer beautifully using markdown suitable for GitHub.
-    DO NOT use any emojis or non-Ascii characters.
-    {agent_scratchpad}
-	`.replaceAll('\n', ' ')
+		const prefix = `
+		YOU MUST LEAVE A COMMENT on the PR according to the user's instructions using the prComment function.
+		You are senior engineer named Maige that is answering questions about a Pull Request on GitHub. Try to keep it somewhat short
+		and don't add fluff to it. Keep the tone neutral and do not include any expressions of gratitude or personal names, Just give the answer.
+		Format your answer in the comment using markdown suitable for GitHub.
+		Don't ask to leave a comment, you must do it automatically.
+		DO NOT use any emojis or non-Ascii characters.
+		{agent_scratchpad}
+		`.replaceAll('\n', ' ')
 
-	const executor = await initializeAgentExecutorWithOptions(tools, model, {
-		agentType: 'openai-functions',
-		returnIntermediateSteps: isDev,
-		handleParsingErrors: true,
-		verbose: false,
-		agentArgs: {
-			prefix
-		}
-	})
+		const executor = await initializeAgentExecutorWithOptions(tools, model, {
+			agentType: 'openai-functions',
+			returnIntermediateSteps: isDev,
+			handleParsingErrors: true,
+			verbose: false,
+			agentArgs: {
+				prefix
+			}
+		})
 
-	const result = await executor.call({input})
-	const {output} = result
+		const result = await executor.call({input})
+		const {output} = result
 
-	return output
+		return output
+	} else {
+		const prefix = `
+		You are reviewing code snippets from a pull request. Your goal is to provide feedback on the code snippet
+		through using the codeComment function. You must specify the line number for the line you wish to comment on. If there is no reason
+		to comment on a line then don't do anything.
+		{agent_scratchpad}
+		`.replaceAll('\n', ' ')
+
+		var files = parse(input)
+		files.forEach(function (file: any) {
+			file.chunks.forEach(async function (chunk: any) {
+				var changes = ''
+				chunk.changes.forEach(function (change: any) {
+					changes += `${change.ln2 === undefined ? change.ln : change.ln2} ${
+						change.content
+					}\n`
+				})
+
+				const tools = [
+					codeComment({
+						octokit,
+						owner,
+						repo,
+						pull_number,
+						commit_id: head,
+						path: file.from
+					})
+				]
+
+				const executor = await initializeAgentExecutorWithOptions(tools, model, {
+					agentType: 'openai-functions',
+					returnIntermediateSteps: isDev,
+					handleParsingErrors: true,
+					verbose: false,
+					agentArgs: {
+						prefix
+					}
+				})
+
+				await executor.call({input: changes})
+			})
+		})
+		return
+	}
 }
