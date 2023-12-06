54179b87cbbcc894a83dabd7e36af5e437749783
arihanv <arihanvaranasi@gmail.com>
2023-12-05 20:52:00 -0600

    feat: remove severity from prComment

--- lib/tools/prComment.ts
+++ lib/tools/prComment.ts
@@ -8,22 +8,18 @@ import {addComment} from '~/utils/github'
 export function prComment({octokit, pullId}: {octokit: any; pullId: string}) {
 	return new DynamicStructuredTool({
 		description: 'Adds a comment to a PR',
-		func: async ({comment, severity}) => {
+		func: async ({comment}) => {
 			const footer = `By [Maige](https://maige.app). How's my driving?`
 			const res = await addComment({
 				octokit,
 				issueId: pullId,
-				comment: `${comment}\nThis is a Level ${severity} problem.\n\n${footer}`
+				comment: `${comment}\n\n${footer}`
 			})
 			return JSON.stringify(res)
 		},
 		name: 'prComment',
 		schema: z.object({
 			comment: z.string().describe('The comment to add'),
-			severity: z
-				.number()
-				.optional()
-				.describe('The severity of the needed fix on a scale of 1-5')
 		})
 	})
 }
