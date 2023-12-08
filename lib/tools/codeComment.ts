54dbda1ca9ac149c0dd82a1792a31f171813bde5
tedspare <ted.spare@gmail.com>
2023-12-08 12:42:55 -0500

    Parametrize code comment filepath

--- lib/tools/codeComment.ts
+++ lib/tools/codeComment.ts
@@ -9,26 +9,24 @@ export function codeComment({
 	octokit,
 	repoFullName,
 	pullNumber,
-	commitId,
-	path
+	commitId
 }: {
 	octokit: any
 	repoFullName: string
 	pullNumber: number
 	commitId: string
-	path: string
 }) {
 	return new DynamicStructuredTool({
 		description: 'Adds a comment to code in a PR',
-		func: async ({comment, line, side}) => {
+		func: async ({comment, line, side, path}) => {
 			const res = await octokit.request(
 				`POST /repos/${repoFullName}/pulls/${pullNumber}/comments`,
 				{
 					body: `${comment}\n\n${COPY.FOOTER}`,
 					commit_id: commitId,
-					path: path,
-					line: line,
-					side: side,
+					path,
+					line,
+					side,
 					headers: {
 						'X-GitHub-Api-Version': '2022-11-28'
 					}
@@ -40,7 +38,10 @@ export function codeComment({
 		schema: z.object({
 			comment: z.string().describe('The comment to add'),
 			line: z.number().describe('The line number'),
-			side: z.enum(['LEFT', 'RIGHT']).describe('If Deletion then LEFT, else RIGHT')
+			side: z
+				.enum(['LEFT', 'RIGHT'])
+				.describe('If Deletion then LEFT, else RIGHT'),
+			path: z.string().describe('The path to the file')
 		})
 	})
 }
