198bc6e34d07f8ed039bb1637ee3ca4231e888e8
arihanv <arihanvaranasi@gmail.com>
2023-12-13 11:28:18 -0600

    Shorten prompt

--- lib/utils/embeddings/summary.ts
+++ lib/utils/embeddings/summary.ts
@@ -3,15 +3,13 @@ import {OpenAI} from 'langchain/llms/openai'
 import {PromptTemplate} from 'langchain/prompts'
 
 const prompt = PromptTemplate.fromTemplate(
-	`You are a helpful assistant that writes a description of the given code snippet. My ultimate goal is to use your produced summary as a key in a key value store vectordb. Your summary is the key and the value is the code.
-    1) Don't mention "code snippet" or "summary" in your response. Just produce the summary of just the code without fluff.
-    2) Try to summarize the code in no more than 6 sentences and use as many keywords as possible.
-    3) Your response should be a single line of text.
+	`You are AI that writes description of code snippet. Follow format of example below. Lots of keywords, straight to the point, one line.
 
-    Example Code: #!/bin/sh\ngroovyc src/*.groovy\ngroovy src/Main.groovy --cp src/
-    Good Summary: Compiles and runs a Groovy program using the source files in the \"src\" directory.
+    Code:#!/bin/sh\ngroovyc src/*.groovy\ngroovy src/Main.groovy --cp src/
+    AI:Compiles and runs a Groovy program using the source files in the \"src\" directory.
 
-    {code}`
+    Code:{code}
+    AI:`
 )
 
 export async function AISummary(code: string, modelName: string = 'gpt-3.5-turbo', temperature: number = 0) {
