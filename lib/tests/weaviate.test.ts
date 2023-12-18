654c469cf0477e2d09d6f6d09132fdcaac33e7a6
tedspare <ted.spare@gmail.com>
2023-12-18 14:04:52 -0500

    Fix test

--- lib/tests/weaviate.test.ts
+++ lib/tests/weaviate.test.ts
@@ -1,21 +1,13 @@
 import {expect, test} from 'bun:test'
 import Weaviate from '~/utils/embeddings/db'
 
-test.skip('Bun test runner - Weaviate', () => {
-	// This is arbitrary. Could just return true.
-
-	expect(Bun.version).toInclude('1.0')
-})
-
-// If Bun is unable to access env vars, pass these to the test runner:
-// WEAVIATE_HOST, WEAVIATE_SCHEME, OPENAI_API_KEY
 test(
 	'Embed repo',
 	async () => {
 		const customerId = 'clot5gx6a0000uvdovvfi1x9q'
 		const repoUrl = 'https://github.com/RubricLab/maige'
 		const branch = 'main'
-		const query = 'codeSearch' // arbitrary - should change over time
+		const query = 'search code' // arbitrary - should change over time
 
 		const vectorDB = new Weaviate(customerId, 'CodeSearch_Test')
 		const docs = await vectorDB.embedRepo(repoUrl, branch)
