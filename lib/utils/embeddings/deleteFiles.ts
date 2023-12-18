a698d67a80781181762c372a9ae13800f09ff3a6
arihanv <arihanvaranasi@gmail.com>
2023-12-18 17:21:12 +0300

    Delete specific files

---
+++ lib/utils/embeddings/deleteFiles.ts
@@ -0,0 +1,40 @@
+import {type WeaviateConfig} from './db'
+
+export default async function deleteFiles(
+	weaviateConfig: WeaviateConfig,
+	repository: string,
+    filePaths: string[],
+    branch: string
+) {
+	const res = await weaviateConfig.client.batch
+		.objectsBatchDeleter()
+		.withClassName(weaviateConfig.indexName)
+		.withWhere({
+			operator: 'And',
+			operands: [
+				{
+					path: ['userId'],
+					operator: 'Equal',
+					valueText: weaviateConfig.userId
+				},
+				{
+					path: ['repository'],
+					operator: 'Equal',
+					valueString: repository
+				},
+                {
+                    path: ['branch'],
+                    operator: "Equal",
+                    valueText: branch
+                },
+                {
+                    path: ['source'],
+                    operator: "ContainsAny",
+                    valueTextArray: filePaths
+                }
+			]
+		})
+		.do()
+
+	return res.results
+}
