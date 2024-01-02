bf1ef482411b0489915d8e8ab7c4ee813286b0b8
arihanv <arihanvaranasi@gmail.com>
2024-01-02 13:30:15 +0530

    User profile dropdown

--- lib/components/dashboard/Navigation/MainNavigation.tsx
+++ lib/components/dashboard/Navigation/MainNavigation.tsx
@@ -3,10 +3,14 @@
 import {Session} from 'next-auth'
 import {signOut} from 'next-auth/react'
 import Image from 'next/image'
-import {useState} from 'react'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger
+} from '~/components/ui/dropdown-menu'
 import MaigeLogo from '../../../../public/logo.png'
-import {PrimaryButton} from '../Buttons'
-import {SmallHeading, Subtext} from '../Text'
 
 export function MainNavigation({
 	session,
@@ -15,35 +19,35 @@ export function MainNavigation({
 	session: Session
 	avatarUrl: string
 }) {
-	const [dropdownOpen, setDropdownOpen] = useState(false)
 	return (
-		<div className='flex w-full flex-row items-center justify-between pb-10'>
+		<div className='sticky top-0 flex w-full select-none flex-row items-center justify-between pb-10 pt-4'>
 			<Image
 				src={MaigeLogo}
-				width={50}
-				height={50}
+				width={40}
+				height={40}
 				className='rounded-full object-cover'
 				alt='Maige Logo'
 			/>
-			<div className='relative'>
-				<Image
-					src={avatarUrl}
-					width={50}
-					height={50}
-					className='cursor-pointer rounded-full object-cover'
-					onClick={() => setDropdownOpen(!dropdownOpen)}
-					alt='User avatar'
-				/>
-				{dropdownOpen && (
-					<div className='bg-panel border-panel-border absolute right-1/2 rounded-lg border-2 p-8'>
-						<div className='pb-6'>
-							<SmallHeading>{session.user.name}</SmallHeading>
-							<Subtext>{session.user.email}</Subtext>
-						</div>
-						<PrimaryButton onClick={() => signOut()}>Sign out</PrimaryButton>
-					</div>
-				)}
-			</div>
+			<DropdownMenu>
+				<DropdownMenuTrigger className='focus-visible:outline-none'>
+					<Image
+						src={avatarUrl}
+						width={40}
+						height={40}
+						className='rounded-full object-cover'
+						alt='User avatar'
+					/>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent className='mr-6 mt-2 w-[235px]'>
+					<DropdownMenuItem disabled={true}>{session.user.email}</DropdownMenuItem>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem
+						className='cursor-pointer'
+						onClick={() => signOut()}>
+						Sign out
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
 		</div>
 	)
 }
