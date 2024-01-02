c6742d0126c40256f3fc8aabcd97f515e737a67e
arihanv <arihanvaranasi@gmail.com>
2024-01-02 22:42:34 +0530

    style: command bar cmds

--- lib/components/command-bar.tsx
+++ lib/components/command-bar.tsx
@@ -1,66 +1,62 @@
-import React from "react"
-import {
-  Calculator,
-  Calendar,
-  CreditCard,
-  Search as SearchIcon,
-  Settings,
-  Smile,
-  User,
-} from "lucide-react"
+import {CommandIcon, Search as SearchIcon} from 'lucide-react'
+import React from 'react'
 
-import { Button } from "~/components/ui/button"
+import {Button} from '~/components/ui/button'
 import {
-  Command,
-  CommandDialog,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-  CommandShortcut,
-} from "~/components/ui/command"
+	CommandDialog,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList
+} from '~/components/ui/command'
 
 export function CommandMenu() {
-  const [open, setOpen] = React.useState(false)
+	const [open, setOpen] = React.useState(false)
 
-  React.useEffect(() => {
-    const down = (e: KeyboardEvent) => {
-      if (e.key === "k" && e.metaKey)  setOpen((open) => !open)
-    }
-    document.addEventListener("keydown", down)
-    return () => document.removeEventListener("keydown", down)
-  }, [])
+	React.useEffect(() => {
+		const down = (e: KeyboardEvent) => {
+			if (e.key === 'k' && e.metaKey) setOpen(open => !open)
+		}
+		document.addEventListener('keydown', down)
+		return () => document.removeEventListener('keydown', down)
+	}, [])
 
-  return (
-    <>
-      <Button
-        asChild
-        variant="outline"
-        onClick={() => setOpen(true)}
-        className="w-full pl-4 pr-2 md:w-80"
-      >
-        <div className="flex h-10 flex-row justify-between">
-          <div className="flex flex-row gap-2">
-            <SearchIcon className="my-auto h-4 w-4" />
-            Search...
-          </div>
-          <kbd className="hidden rounded bg-muted px-[0.4rem] py-[0.2rem] font-mono text-xs sm:block">
-          ⌘ k
-          </kbd>
-        </div>
-      </Button>
-      <CommandDialog open={open} onOpenChange={setOpen}>
-        <CommandInput placeholder="Type a command or search..." />
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Suggestions">
-            <CommandItem>Need to add stuff</CommandItem>
-            <CommandItem>example</CommandItem>
-          </CommandGroup>
-        </CommandList>
-      </CommandDialog>
-    </>
-  )
+	return (
+		<>
+			<Button
+				asChild
+				variant='outline'
+				onClick={() => setOpen(true)}
+				className='w-full pl-4 pr-2 md:w-80'>
+				<div className='flex h-10 flex-row justify-between'>
+					<div className='flex flex-row gap-2'>
+						<SearchIcon className='my-auto h-4 w-4' />
+						Search...
+					</div>
+					<kbd className='hidden items-center gap-1 px-[0.2rem] py-[0.2rem] font-mono sm:inline-flex'>
+						<span className='flex h-[25px] w-[25px] items-center justify-center rounded bg-muted'>
+							<CommandIcon width={12} />
+						</span>{' '}
+						<span className='flex h-[25px] w-[25px] items-center justify-center rounded bg-muted text-sm'>
+             {/* Attempts to center the K */}
+							<span className='mt-[0.05rem]'>K</span>
+						</span>
+					</kbd>
+				</div>
+			</Button>
+			<CommandDialog
+				open={open}
+				onOpenChange={setOpen}>
+				<CommandInput placeholder='Type a command or search...' />
+				<CommandList>
+					<CommandEmpty>No results found.</CommandEmpty>
+					<CommandGroup heading='Suggestions'>
+						<CommandItem>Need to add stuff</CommandItem>
+						<CommandItem>example</CommandItem>
+					</CommandGroup>
+				</CommandList>
+			</CommandDialog>
+		</>
+	)
 }
