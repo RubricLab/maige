8a3acfdfaa7c5253f12c834aa1375298c00d6558
tedspare <ted.spare@gmail.com>
2024-01-13 13:48:12 -0500

    Bump repo count

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ const Page = () => {
 						</div>
 					</div>
 					<div className='flex flex-col items-center gap-2 py-8'>
-						<p className='text-sm text-white/60'>Used by 1.5k+ repos</p>
+						<p className='text-sm text-white/60'>Used by 1.7k+ repos</p>
 						<div className='flex flex-row flex-wrap items-center justify-center gap-5 text-white/60'>
 							<Highlight className='h-6 hover:text-white' />
 							<Precedent className='group h-6 hover:text-white' />
