03129cf11df8bbcae22365f8d9edf7df164f35ba
tedspare <ted.spare@gmail.com>
2024-01-13 13:24:49 -0500

    Move dashboard auth to GH App. Re-style login page.

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -2,7 +2,7 @@ import {Landing} from '~/components/dashboard/Landing'
 
 type Props = {}
 
-export default function SignIN({}: Props) {
+export default function SignIn({}: Props) {
 	return (
 		<div>
 			<Landing />
