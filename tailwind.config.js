cfbf70020659a965d08988b68a5161df73ac77b3
Sarim Malik <sarim@rubriclabs.com>
2024-01-17 12:05:10 -0500

    Create util for fonts, update fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -63,6 +63,9 @@ module.exports = {
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
 			},
+			fontFamily: {
+				italiana: ['var(--font-italiana)']
+			},
 			keyframes: {
 				'accordion-down': {
 					from: {height: '0'},
