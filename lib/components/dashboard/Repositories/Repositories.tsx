4f85cad36806f1e99437aa9f33961560ff4a5a92
Sarim Malik <sarim@rubriclabs.com>
2024-01-20 13:53:16 -0500

    Ensure repo card is clickable

--- lib/components/dashboard/Repositories/Repositories.tsx
+++ lib/components/dashboard/Repositories/Repositories.tsx
@@ -15,14 +15,14 @@ export function Repositories({projects}: {projects: any[]}) {
 		<div className='grid w-full gap-4 sm:grid-cols-4'>
 			{projects.map(project => (
 				<Link
-					className='flex h-36 w-full cursor-pointer flex-col justify-between rounded-lg border-2 border-border border-opacity-20 p-4 py-3.5 transition-all hover:border-opacity-100'
+					className='relative flex h-36 w-full cursor-pointer flex-col justify-between rounded-lg border-2 border-border border-opacity-20 p-4 py-3.5 transition-all hover:border-opacity-100'
 					href={`/dashboard/repo/${project.id}`}
 					key={project.id}>
 					<div className='flex w-full items-center justify-between'>
 						<div className='flex items-center gap-3'>
 							<div className='relative'>
-								<div className='h-6 w-6 rounded-full' />
-								<p className='absolute left-0 right-0 top-1/2 m-auto -translate-y-1/2 text-center font-semibold leading-none text-secondary'>
+								<div className='h-6 w-6 rounded-full bg-foreground' />
+								<p className='absolute left-0 right-0 top-1/2 m-auto -translate-y-1/2 text-center font-medium leading-none text-secondary'>
 									{project.name.charAt(0).toUpperCase()}
 								</p>
 							</div>
