d711c94a018c0866c2f31628a2493c15fe01f030
tedspare <ted.spare@gmail.com>
2024-01-23 18:32:24 -0500

    Remove faces from mockups

--- lib/components/logos/Maige.tsx
+++ lib/components/logos/Maige.tsx
@@ -1,8 +1,15 @@
-export const Maige = ({className}: {className?: string}) => {
+export const Maige = ({
+	className,
+	style
+}: {
+	className?: string
+	style?: React.CSSProperties
+}) => {
 	return (
 		<svg
 			viewBox='0 0 500 500'
 			className={className}
+			style={style}
 			fill='none'
 			xmlns='http://www.w3.org/2000/svg'>
 			<path
