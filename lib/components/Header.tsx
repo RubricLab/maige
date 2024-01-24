48a7fc938f80f8a97d3f3845fca1c74af64c347a
Sarim Malik <sarim@rubriclabs.com>
2024-01-23 23:54:59 -0500

    Add alpha tag

--- lib/components/Header.tsx
+++ lib/components/Header.tsx
@@ -4,13 +4,18 @@ import {Maige} from './logos'
 
 export const Header = () => {
 	return (
-		<header className='fixed top-0 z-20 flex w-screen items-center justify-start bg-primary p-4'>
+		<header className='bg-primary fixed top-0 z-20 flex w-screen items-center justify-start p-4'>
 			<Link
 				href='/'
-				className='flex items-baseline gap-2 text-primary opacity-80 transition-all hover:text-orange-200 hover:opacity-100 dark:text-orange-100'>
-				<Maige className='h-8' />
-				<div className='font-monocraft text-3xl font-bold !tracking-tighter'>
-					maige
+				className='text-primary group flex flex-col'>
+				<div className='flex items-baseline gap-2 opacity-80 transition-all hover:text-orange-200 hover:opacity-100 dark:text-orange-100'>
+					<Maige className='h-8' />
+					<div className='font-monocraft text-3xl font-bold !tracking-tighter'>
+						maige
+					</div>
+				</div>
+				<div className='flex justify-end'>
+					<span className='text-sm opacity-80 dark:text-orange-100'>alpha</span>
 				</div>
 			</Link>
 			<div className='grow'>{/* Divider */}</div>
