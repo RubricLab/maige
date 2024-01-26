3096d9f61b84c2a56d7c8bfe5e4f55114dad4f81
Sarim Malik <sarim@rubriclabs.com>
2024-01-26 01:34:14 -0500

    Add util for getting current user

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,13 +1,9 @@
-import {DefaultSession} from 'next-auth'
+import 'next-auth'
 
 declare module 'next-auth' {
-	/**
-	 * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-	 */
 	interface Session {
-		user: {
-			/** The user's githuber User ID */
-			githubUserId: string
-		} & DefaultSession['user']
+		user: User & {
+			id: UserId
+		}
 	}
 }
