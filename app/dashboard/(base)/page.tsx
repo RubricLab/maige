eea4d0ef10ead9c27b561643033a9444b26afdef
Sarim Malik <sarim@rubriclabs.com>
2024-01-25 21:30:58 -0500

    Add prisma adapter. Remove unused code. Add session & accounts data in database.

--- app/dashboard/(base)/page.tsx
+++ app/dashboard/(base)/page.tsx
@@ -2,38 +2,35 @@ import {getServerSession} from 'next-auth'
 import {redirect} from 'next/navigation'
 import {Suspense} from 'react'
 import {authOptions} from '~/authOptions'
-import {Repositories} from '~/components/dashboard/Repositories'
-import prisma from '~/prisma'
 
 export default async function Page() {
 	const session = await getServerSession(authOptions)
+	if (!session) redirect('/')
 
-	if (!session?.user?.githubUserId) redirect('/')
+	// const customer = await prisma.customer.findUnique({
+	// 	where: {
+	// 		githubUserId: session.user.githubUserId
+	// 	}
+	// })
 
-	const customer = await prisma.customer.findUnique({
-		where: {
-			githubUserId: session.user.githubUserId
-		}
-	})
-
-	const projects = customer
-		? await prisma.project.findMany({
-				where: {
-					customerId: customer.id
-				},
-				select: {
-					id: true,
-					name: true,
-					createdAt: true,
-					customInstructions: true
-				}
-			})
-		: []
+	// const projects = customer
+	// 	? await prisma.project.findMany({
+	// 			where: {
+	// 				customerId: customer.id
+	// 			},
+	// 			select: {
+	// 				id: true,
+	// 				name: true,
+	// 				createdAt: true,
+	// 				customInstructions: true
+	// 			}
+	// 		})
+	// 	: []
 
 	return (
 		<div className='flex flex-col items-center'>
 			<Suspense fallback={<p>Loading...</p>}>
-				<Repositories projects={projects} />
+				{/* <Repositories projects={projects} /> */}
 			</Suspense>
 		</div>
 	)
--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,42 +1,15 @@
-import {AuthOptions, Profile} from 'next-auth'
-import GithubProvider, {GithubProfile} from 'next-auth/providers/github'
+import {PrismaAdapter} from '@auth/prisma-adapter'
+import {AuthOptions} from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
 import prisma from '~/prisma'
 import env from './env.mjs'
 
 export const authOptions: AuthOptions = {
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		GithubProvider({
 			clientId: env.GITHUB_CLIENT_ID as string,
-			clientSecret: env.GITHUB_CLIENT_SECRET as string,
-			profile(profile: GithubProfile) {
-				return {
-					id: profile.id.toString(),
-					name: profile.name,
-					userName: profile.login,
-					email: profile.email,
-					image: profile.avatar_url
-				}
-			}
+			clientSecret: env.GITHUB_CLIENT_SECRET as string
 		})
-	],
-	// attach the githubUserId that we store server side below to our session
-	callbacks: {
-		async session({session, token}) {
-			session.user.githubUserId = token.sub
-			return session
-		}
-	},
-	// by design, NextAuth does not expose a user's Github username to the client and in our session object, only their github numeric user ID. So, we need to store this numeric ID in our DB
-	// this is so we can know who is making the request on the server
-	events: {
-		async signIn({account, profile}) {
-			const typedProfile: Profile & {userName?: string} = profile
-			await prisma.customer.update({
-				where: {name: typedProfile.userName},
-				data: {
-					githubUserId: account.providerAccountId
-				}
-			})
-		}
-	}
+	]
 }
