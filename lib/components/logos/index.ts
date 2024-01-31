7edffc8d0ff9956410cd17fbbf6b8a20d225b3ad
tedspare <ted.spare@gmail.com>
2024-01-31 16:33:59 -0500

    Update landing page

--- lib/components/logos/index.ts
+++ lib/components/logos/index.ts
@@ -1,8 +1,9 @@
 import {Cal} from './Cal'
+import {Documenso} from './Documenso'
 import {Highlight} from './Highlight'
 import {Maige} from './Maige'
 import {Nuxt} from './Nuxt'
 import {Precedent} from './Precedent'
 import {Trigger} from './Trigger'
 
-export {Cal, Highlight, Maige, Nuxt, Precedent, Trigger}
+export {Cal, Documenso, Highlight, Maige, Nuxt, Precedent, Trigger}
