616f96abf17bc3524cdf209698767822a92c8495
Dexter Storey <36115192+DexterStorey@users.noreply.github.com>
2024-01-31 01:41:04 -0500

    typesafe auth refactor (#103)
    * typesafe auth
    
    * ExtendedUser, ExtendedSessionUser interface improvements
    
    * last looks
    
    * omit id, add githubUserId
    
    * simplified
    
    * omit id from profile
    
    * add usage to session
    
    * add back provider Id

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,13 +1,12 @@
 import {PrismaAdapter} from '@auth/prisma-adapter'
-import {AuthOptions} from 'next-auth'
-import {AdapterUser} from 'next-auth/adapters'
-import GithubProvider, {type GithubProfile} from 'next-auth/providers/github'
+import {AuthOptions, Profile} from 'next-auth'
+import GithubProvider, {GithubProfile} from 'next-auth/providers/github'
 import prisma from '~/prisma'
 import env from './env.mjs'
 
 const prismaAdapter = PrismaAdapter(prisma)
 
-prismaAdapter.createUser = (data: AdapterUser & {userName: string}) => {
+prismaAdapter.createUser = data => {
 	return prisma.user.upsert({
 		// migration flow
 		where: {userName: data.userName},
@@ -24,10 +23,11 @@ export const authOptions: AuthOptions = {
 		GithubProvider({
 			clientId: env.GITHUB_CLIENT_ID as string,
 			clientSecret: env.GITHUB_CLIENT_SECRET as string,
-			profile(profile: GithubProfile) {
+			profile(profile: GithubProfile): Profile {
 				// Add user profile information
 				return {
 					id: profile.id.toString(),
+					githubUserId: profile.id.toString(),
 					name: profile.name,
 					email: profile.email,
 					image: profile.avatar_url,
@@ -39,7 +39,8 @@ export const authOptions: AuthOptions = {
 	callbacks: {
 		session: async ({session, user}) => {
 			session.user.id = user.id
-			session.user.userName = (user as AdapterUser & {userName: string}).userName
+			session.user.userName = user.userName
+			session.user.usage = user.usage
 			return session
 		}
 	}
