136427d0009a41d1bbca685dd0d8c7907dbbcb2e
DexterStorey <dexter@dexterstorey.com>
2024-01-30 22:50:03 -0500

    typesafe auth

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,13 +1,13 @@
 import {PrismaAdapter} from '@auth/prisma-adapter'
+import type {Profile} from 'next-auth'
 import {AuthOptions} from 'next-auth'
-import {AdapterUser} from 'next-auth/adapters'
 import GithubProvider, {type GithubProfile} from 'next-auth/providers/github'
 import prisma from '~/prisma'
 import env from './env.mjs'
 
 const prismaAdapter = PrismaAdapter(prisma)
 
-prismaAdapter.createUser = (data: AdapterUser & {userName: string}) => {
+prismaAdapter.createUser = data => {
 	return prisma.user.upsert({
 		// migration flow
 		where: {userName: data.userName},
@@ -24,7 +24,7 @@ export const authOptions: AuthOptions = {
 		GithubProvider({
 			clientId: env.GITHUB_CLIENT_ID as string,
 			clientSecret: env.GITHUB_CLIENT_SECRET as string,
-			profile(profile: GithubProfile) {
+			profile(profile: GithubProfile): Profile {
 				// Add user profile information
 				return {
 					id: profile.id.toString(),
@@ -39,7 +39,7 @@ export const authOptions: AuthOptions = {
 	callbacks: {
 		session: async ({session, user}) => {
 			session.user.id = user.id
-			session.user.userName = (user as AdapterUser & {userName: string}).userName
+			session.user.userName = user.userName
 			return session
 		}
 	}
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,22 +1,27 @@
 import 'next-auth'
-import {DefaultSession} from 'next-auth'
+import type {Profile as DefaultProfile} from 'next-auth'
+import {DefaultSession, DefaultUser} from 'next-auth'
+import type {AdapterUser as DefaultAdapterUser} from 'next-auth/adapters'
+
+// Make changes to the ExtendedUser interface to see them in session, profile, DB
+interface ExtendedUser extends DefaultUser {
+	userName: string
+}
 
 declare module 'next-auth' {
 	/**
 	 * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
 	 */
-	interface Session {
-		user: {
-			id: string
-			userName: string
-		} & DefaultSession['user']
+
+	interface User extends ExtendedUser {}
+
+	interface Session extends DefaultSession {
+		user: User
 	}
 
-	interface Profile {
-		id: string
-		name: string
-		email: string
-		image: string
-		userName: string
-	}
+	interface Profile extends DefaultProfile, ExtendedUser {}
+}
+
+declare module '@auth/core/adapters' {
+	interface AdapterUser extends DefaultAdapterUser, ExtendedUser {}
 }
