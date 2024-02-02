eef6df9b7f26e164b08b5856786927e57f868b77
tedspare <ted.spare@gmail.com>
2024-02-02 12:00:28 -0500

    Move add project button to start

--- lib/components/dashboard/Projects/ProjectsList.tsx
+++ lib/components/dashboard/Projects/ProjectsList.tsx
@@ -28,10 +28,11 @@ export default function ProjectsList({
 }) {
 	return (
 		<div className='grid w-full gap-4 sm:grid-cols-4'>
+			<CreateProjectIntent teamId={teamId} />
 			{projects.length > 0 ? (
 				projects.map(project => (
 					<Link
-						className='border-tertiary hover:border-secondary relative flex h-36 w-full cursor-pointer flex-col justify-between rounded-sm border p-4 py-3.5 transition-opacity duration-300'
+						className='border-tertiary hover:border-secondary relative flex h-36 w-full cursor-pointer flex-col justify-between rounded-sm border p-4 py-3.5 transition-all'
 						href={`${slug}/project/${project.id}/instructions`} // TODO: remove after populating Overview tab
 						key={project.id}>
 						<div className='flex w-full items-center justify-between'>
@@ -58,7 +59,6 @@ export default function ProjectsList({
 			) : (
 				<EmptyState />
 			)}
-			<CreateProjectIntent teamId={teamId} />
 		</div>
 	)
 }
