e0ce59067e02a2b3e4dd36c0a3c9e27f7569faab
Sarim Malik <sarim@rubriclabs.com>
2024-02-04 13:22:25 -0500

    Isolate setting sections into separate components. Add dialog for inviting someone.

--- app/[slug]/(base)/settings/page.tsx
+++ app/[slug]/(base)/settings/page.tsx
@@ -1,15 +1,23 @@
-import {TrashIcon} from 'lucide-react'
-import {Button} from '~/components/ui/button'
+import {DeleteTeam, InviteDialog} from '~/components/dashboard/Settings'
+import prisma from '~/prisma'
 
-export default function Settings() {
+export default async function Settings({params}: {params: {slug: string}}) {
+	const team = await prisma.team.findUnique({
+		where: {slug: params.slug},
+		select: {id: true}
+	})
 	return (
 		<div className='flex h-full w-full flex-col gap-4'>
-			<h3>Settings</h3>
-			<Button
-				className='w-fit'
-				variant='destructive'>
-				Delete team <TrashIcon className='h-4 w-4' />
-			</Button>
+			<h2 className='font-sans text-2xl'>Settings</h2>
+			<div className='flex items-center gap-2'>
+				<h3>Members</h3>
+				<InviteDialog teamId={team.id} />
+			</div>
+
+			<div className='flex flex-col gap-2'>
+				<h3>Advanced</h3>
+				<DeleteTeam />
+			</div>
 		</div>
 	)
 }
---
+++ lib/components/dashboard/Settings/InviteForm.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import {Dispatch, SetStateAction, useEffect} from 'react'
+import {useFormState, useFormStatus} from 'react-dom'
+import {toast} from 'sonner'
+import createInvitation from '~/actions/create-invitation'
+import {Button} from '~/components/ui/button'
+import {Input} from '~/components/ui/input'
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue
+} from '~/components/ui/select'
+
+const initialState = {
+	type: null,
+	message: null
+}
+
+function InviteButton() {
+	const {pending} = useFormStatus()
+	return (
+		<div className='flex w-full items-center justify-end gap-4'>
+			<Button
+				type='submit'
+				className='w-fit'
+				disabled={pending}>
+				Invite
+			</Button>
+		</div>
+	)
+}
+
+export default function InviteForm({
+	teamId,
+	setDialogOpen
+}: {
+	teamId: string
+	setDialogOpen: Dispatch<SetStateAction<boolean>>
+}) {
+	const [state, formAction] = useFormState(createInvitation, initialState)
+
+	// Trigger toast when state changes
+	useEffect(() => {
+		if (state?.type === 'success') {
+			toast.success(state?.message)
+			setDialogOpen(false)
+		} else if (state?.type === 'error') {
+			toast.error(state?.message)
+			console.error(state?.message)
+		}
+	}, [setDialogOpen, state])
+
+	return (
+		<form
+			className='flex w-full flex-col gap-4'
+			action={formAction}>
+			<h3>Invite member</h3>
+			<Input
+				name='email'
+				type='email'
+				placeholder='Email'
+			/>
+			<Input
+				name='teamId'
+				type='hidden'
+				value={teamId}
+			/>
+			<Select name='role'>
+				<SelectTrigger className='w-[180px]'>
+					<SelectValue placeholder='Role' />
+				</SelectTrigger>
+				<SelectContent>
+					<SelectItem value='ADMIN'>Admin</SelectItem>
+					<SelectItem value='USER'>User</SelectItem>
+				</SelectContent>
+			</Select>
+			<InviteButton />
+		</form>
+	)
+}
