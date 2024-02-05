d9dbb3193c7cfa2449c90af8ea0b824472f27046
Sarim Malik <sarim@rubriclabs.com>
2024-02-05 01:52:26 -0500

    Add options to each table row

--- lib/components/dashboard/Settings/ExistingMembers.tsx
+++ lib/components/dashboard/Settings/ExistingMembers.tsx
@@ -1,4 +1,15 @@
+'use client'
+
 import {Role} from '@prisma/client'
+import {MoreVerticalIcon} from 'lucide-react'
+import {buttonVariants} from '~/components/ui/button'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuPortal,
+	DropdownMenuTrigger
+} from '~/components/ui/dropdown-menu'
 import {convertToTitleCase, parseDate} from '~/utils'
 
 type Member = {
@@ -14,18 +25,32 @@ export default function ExistingMembers({members}: {members: Member[]}) {
 		<div className='flex flex-col gap-2'>
 			<p className='text-xl'>Existing members</p>
 			<div className='rounded-md border'>
-				<div className='border-border grid grid-cols-3 border-b px-4 py-1 text-sm'>
-					<p>Email</p>
+				<div className='border-border grid grid-cols-6 border-b px-4 py-1 text-sm'>
+					<p className='col-span-3'>Email</p>
 					<p>Role</p>
 					<p>Member since</p>
+					<p></p>
 				</div>
 				{members.map(m => (
 					<div
 						key={m.user.email}
-						className='border-border grid grid-cols-3 rounded-sm border-b p-4'>
-						<p>{m.user.email}</p>
+						className='border-border grid grid-cols-6 items-center rounded-sm border-b p-4'>
+						<p className='col-span-3'>{m.user.email}</p>
 						<p>{convertToTitleCase(m.role)}</p>
 						<p>{parseDate(m.user.createdAt)}</p>
+						<div className='flex justify-center'>
+							<DropdownMenu>
+								<DropdownMenuTrigger
+									className={`border-none focus-visible:outline-none ${buttonVariants({variant: 'outline', size: 'icon'})}`}>
+									<MoreVerticalIcon className='h-4 w-4' />
+								</DropdownMenuTrigger>
+								<DropdownMenuPortal>
+									<DropdownMenuContent className='w-fit -translate-x-[40%]'>
+										<DropdownMenuItem onClick={() => {}}>Delete member</DropdownMenuItem>
+									</DropdownMenuContent>
+								</DropdownMenuPortal>
+							</DropdownMenu>
+						</div>
 					</div>
 				))}
 			</div>
