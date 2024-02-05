4768a41f3c26b4d7feb802f47d56beea21c4d985
Sarim Malik <sarim@rubriclabs.com>
2024-02-05 01:33:12 -0500

    Add settings layout and routes

--- app/[slug]/(base)/settings/page.tsx
+++ app/[slug]/(base)/settings/page.tsx
@@ -1,23 +1,5 @@
-import {DeleteTeam, InviteDialog} from '~/components/dashboard/Settings'
-import prisma from '~/prisma'
+import {redirect} from 'next/navigation'
 
 export default async function Settings({params}: {params: {slug: string}}) {
-	const team = await prisma.team.findUnique({
-		where: {slug: params.slug},
-		select: {id: true}
-	})
-	return (
-		<div className='flex h-full w-full flex-col gap-4'>
-			<h2 className='font-sans text-2xl'>Settings</h2>
-			<div className='flex items-center gap-2'>
-				<h3>Members</h3>
-				<InviteDialog teamId={team.id} />
-			</div>
-
-			<div className='flex flex-col gap-2'>
-				<h3>Advanced</h3>
-				<DeleteTeam />
-			</div>
-		</div>
-	)
+	redirect(`/${params.slug}/settings/members`)
 }
