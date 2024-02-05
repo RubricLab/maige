a238dbb8e00c3b59d306ced79caa783aff904fae
Sarim Malik <sarim@rubriclabs.com>
2024-02-05 17:47:26 -0500

    Remove extra changes

--- lib/components/dashboard/Buttons/Sun.tsx
+++ lib/components/dashboard/Buttons/Sun.tsx
@@ -1,10 +1,9 @@
 'use client'
-import {User} from 'next-auth'
 import {signIn} from 'next-auth/react'
 import {Button} from '~/components/ui/button'
 import {cn} from '~/utils'
 
-export const Sun = ({user, className}: {user: User; className?: string}) => {
+export const Sun = ({className}: {className?: string}) => {
 	return (
 		<Button
 			onClick={() => signIn('github', {callbackUrl: '/'})}
@@ -13,7 +12,7 @@ export const Sun = ({user, className}: {user: User; className?: string}) => {
 				'center group h-9 w-9 rounded-full border border-white bg-gradient-to-t from-sunset to-orange-500 drop-shadow-glow transition-all hover:w-32 hover:border-sunset hover:from-white dark:border-black dark:hover:from-black dark:hover:to-black'
 			)}>
 			<span className='whitespace-nowrap text-base opacity-0 transition-opacity group-hover:opacity-100'>
-				{user ? 'Dashboard' : 'Log in'}
+				Log in
 			</span>
 		</Button>
 	)
