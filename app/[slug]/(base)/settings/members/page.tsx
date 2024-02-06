be39010fcb68232823c31e43a64447730a0a9cc8
Sarim Malik <sarim@rubriclabs.com>
2024-02-05 20:56:03 -0500

    Ensure only users in team can see page

--- app/[slug]/(base)/settings/members/page.tsx
+++ app/[slug]/(base)/settings/members/page.tsx
@@ -1,13 +1,18 @@
+import {redirect} from 'next/navigation'
 import {
 	ExistingMembers,
 	InviteDialog,
 	PendingInvitations
 } from '~/components/dashboard/Settings'
 import prisma from '~/prisma'
+import {getCurrentUser} from '~/utils/session'
 
 export default async function Members({params}: {params: {slug: string}}) {
-	const team = await prisma.team.findUnique({
-		where: {slug: params.slug},
+	const user = await getCurrentUser()
+	if (!user) redirect('/')
+
+	const team = await prisma.team.findFirst({
+		where: {slug: params.slug, memberships: {some: {userId: user.id}}},
 		select: {
 			id: true,
 			memberships: {
@@ -23,6 +28,8 @@ export default async function Members({params}: {params: {slug: string}}) {
 			}
 		}
 	})
+	if (!team) redirect('/')
+
 	return (
 		<div className='flex flex-col gap-6 py-0.5'>
 			<div className='flex items-center justify-between gap-2'>
