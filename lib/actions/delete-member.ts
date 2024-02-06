7d45f809aae8602d20469bc7c00fdaf895395dd0
Sarim Malik <sarim@rubriclabs.com>
2024-02-06 13:04:32 -0500

    Streamline deleting a member, cover edge case when an admin trying to delete another admin

--- lib/actions/delete-member.ts
+++ lib/actions/delete-member.ts
@@ -4,12 +4,7 @@ import prisma from '~/prisma'
 import {getCurrentUser} from '~/utils/session'
 
 // Delete invitation to join a team
-export default async function deleteMember(
-	teamId: string,
-	membershipId: string,
-	memberUserId: string,
-	memberEmail: string
-) {
+export default async function deleteMember(membershipId: string) {
 	const user = await getCurrentUser()
 	if (!user)
 		return {
@@ -17,16 +12,34 @@ export default async function deleteMember(
 			type: 'error'
 		}
 
-	// Check membership of user triggering delete
-	const membership = await prisma.membership.findFirst({
-		where: {AND: [{teamId: teamId, userId: user.id}]}
+	// Get the membership record that needs to be deleted
+	const targetMembership = await prisma.membership.findUnique({
+		where: {id: membershipId},
+		include: {user: {select: {id: true, email: true, userName: true}}}
 	})
 
-	// Scenarios in which a request should be disallowed:
+	if (!targetMembership)
+		return {
+			message: 'User does not exist in the team',
+			type: 'error'
+		}
+
+	// Check membership of user triggering delete
+	const userMembership = await prisma.membership.findFirst({
+		where: {AND: [{teamId: targetMembership.teamId, userId: user.id}]}
+	})
+
 	// 1. user making request is not in the team
 	// 2. non-admin user is making request
-	// 3. user is trying to remove themself
-	if (!membership || membership.role === 'USER' || user.id === memberUserId)
+	if (!userMembership || userMembership.role === 'USER')
+		return {
+			message: 'Unauthorized, not allowed to remove member',
+			type: 'error'
+		}
+
+	// 1. attempting to remove an admin
+	// 2. user is trying to remove themself
+	if (targetMembership.role === 'ADMIN' || user.id === targetMembership.user.id)
 		return {
 			message: 'Unauthorized, not allowed to remove member',
 			type: 'error'
@@ -39,7 +52,7 @@ export default async function deleteMember(
 			}
 		})
 		return {
-			message: `Removed ${memberEmail}`,
+			message: `Removed ${targetMembership.user.email ?? targetMembership.user.userName}`,
 			type: 'success'
 		}
 	} catch (err) {
