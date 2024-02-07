1f72b4e6148203271a6e08409cdd2c0a41d623ab
Sarim Malik <sarim@rubriclabs.com>
2024-02-06 22:36:09 -0500

    Add general settings page, add ability to update team name

--- app/[slug]/(base)/settings/general/page.tsx
+++ app/[slug]/(base)/settings/general/page.tsx
@@ -1,7 +1,12 @@
-export default function General({params}: {params: {slug: string}}) {
+import TeamDetailsForm from '~/components/dashboard/Settings/TeamDetailsForm'
+import prisma from '~/prisma'
+
+export default async function General({params}: {params: {slug: string}}) {
+	const team = await prisma.team.findUnique({where: {slug: params.slug}})
 	return (
 		<div className='flex flex-col gap-6 py-0.5'>
 			<h3>General</h3>
+			<TeamDetailsForm team={team} />
 		</div>
 	)
 }
