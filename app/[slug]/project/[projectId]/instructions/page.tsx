ab5099b8f5caab88826527a207996d8b6e5976e4
tedspare <ted.spare@gmail.com>
2024-02-09 10:42:44 -0500

    Add instructions to instructions page

--- app/[slug]/project/[projectId]/instructions/page.tsx
+++ app/[slug]/project/[projectId]/instructions/page.tsx
@@ -10,7 +10,13 @@ export default async function Page({
 	return (
 		<Suspense fallback={<PanelSkeletons amount={3} />}>
 			<div className='flex w-full flex-col gap-4'>
-				<h3>Instructions</h3>
+				<div className='gap-2'>
+					<h3>Instructions</h3>
+					<p className='text-tertiary text-base'>
+						Write rules for what Maige should do when an issue, PR, or comment comes
+						in.
+					</p>
+				</div>
 				<Instructions projectId={params.projectId} />
 			</div>
 		</Suspense>
