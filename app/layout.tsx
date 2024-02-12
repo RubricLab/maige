1c2529a9cca0113969f14f79fa2fc1548dd4a7e4
tedspare <ted.spare@gmail.com>
2024-02-12 10:43:30 -0500

    Fix social preview image links

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,6 @@ import {jakarta, monocraft, roboto} from '~/utils/fonts'
 
 const title = 'Maige'
 const description = 'AI-powered codebase actions.'
-const preview = {
-	url: '/preview.png',
-	width: 1200,
-	height: 630,
-	alt: 'Maige logo'
-}
 
 export const viewport: Viewport = {
 	themeColor: '#000'
@@ -31,14 +25,12 @@ export const metadata: Metadata = {
 		description,
 		url: 'maige.app',
 		siteName: title,
-		type: 'website',
-		images: preview
+		type: 'website'
 	},
 	twitter: {
 		title,
 		creator: '@RubricLabs',
 		card: 'summary_large_image',
-		images: preview,
 		description
 	}
 }
