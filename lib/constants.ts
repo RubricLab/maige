8c2784bef6182e41895f7d2c489e143798d11245
Sarim Malik <sarim@rubriclabs.com>
2024-02-18 20:44:54 -0500

    Add resend

--- lib/constants.ts
+++ lib/constants.ts
@@ -12,3 +12,11 @@ export const MAX_BODY_LENGTH = 2000
 export const COPY = {
 	FOOTER: `<sub>From [Maige](https://maige.app). How's my driving?</sub>`
 }
+
+export const DOMAIN = {
+	PROD: 'https://maige.app'
+}
+
+export const EMAIL = {
+	FROM: 'Maige <hey@mail.maige.app>'
+}
