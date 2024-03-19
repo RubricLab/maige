27b87f392e3fc01110f5793fc4c71376d6ae4dc9
Ted Spare <ted.spare@gmail.com>
2024-03-19 14:43:33 -0400

    Update Logos.tsx

--- lib/components/logos/Logos.tsx
+++ lib/components/logos/Logos.tsx
@@ -3,7 +3,7 @@ import {Cal, Documenso, Highlight, Nuxt, Precedent, Trigger} from './'
 export const Logos = () => {
 	return (
 		<>
-			<p className='text-tertiary font-monocraft'>Used by 2.2k++ repos:</p>
+			<p className='text-tertiary font-monocraft'>Used by 2.4k++ repos:</p>
 			<div className='center gap-12 sm:gap-14'>
 				<Documenso className='h-8 opacity-60 transition-opacity hover:opacity-100' />
 				<Nuxt className='h-8 opacity-60 transition-opacity hover:opacity-100' />
