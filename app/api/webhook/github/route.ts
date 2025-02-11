eccd00aacb081c400ba407e08033df95bb3d6674
tedspare <ted.spare@gmail.com>
2025-02-10 19:51:58 -0500

    Add logs

--- app/api/webhook/github/route.ts
+++ app/api/webhook/github/route.ts
@@ -12,10 +12,6 @@ import {
 
 export const maxDuration = 300
 
-const webhook = new Webhooks({
-	secret: env.GITHUB_WEBHOOK_SECRET
-})
-
 /**
  * POST /api/webhook
  *
@@ -23,6 +19,9 @@ const webhook = new Webhooks({
  */
 export const POST = async (req: Request) => {
 	const hdrs = headers()
+	const webhook = new Webhooks({
+		secret: env.GITHUB_WEBHOOK_SECRET
+	})
 
 	// Verify webhook signature
 	await webhook
@@ -41,26 +40,31 @@ export const POST = async (req: Request) => {
 			})
 		})
 
-	console.log('Webhook event received:', hdrs.get('x-github-event'))
+	try {
+		// Handle webhooks
+		// @ts-ignore
+		webhook.on('push', handlePush)
+		// @ts-ignore
+		webhook.on('installation.created', handleAppInstall)
+		webhook.on(
+			['installation_repositories.added', 'installation_repositories.removed'],
+			// @ts-ignore
+			handleAppUpdates
+		)
+		// @ts-ignore
+		webhook.on('installation.deleted', handleAppUnInstall)
+		// @ts-ignore
+		webhook.on(['issues.opened', 'issue_comment.created'], handleIssues)
+		// @ts-ignore
+		webhook.on(['pull_request'], handlePullRequests)
 
-	await new Promise(resolve => setTimeout(resolve, 5000))
+		// TODO: remove
+		await new Promise(resolve => setTimeout(resolve, 10_000))
 
-	return new Response('Webhook received', { status: 203 })
+		return new Response('Webhook received', { status: 203 })
+	} catch (err) {
+		console.log('Error in webhook endpoint')
+		console.error(err)
+		return new Response('Webhook error', { status: 500 })
+	}
 }
-
-// Handle webhooks
-// @ts-ignore
-webhook.on('push', handlePush)
-// @ts-ignore
-webhook.on('installation.created', handleAppInstall)
-webhook.on(
-	['installation_repositories.added', 'installation_repositories.removed'],
-	// @ts-ignore
-	handleAppUpdates
-)
-// @ts-ignore
-webhook.on('installation.deleted', handleAppUnInstall)
-// @ts-ignore
-webhook.on(['issues.opened', 'issue_comment.created'], handleIssues)
-// @ts-ignore
-webhook.on(['pull_request'], handlePullRequests)
