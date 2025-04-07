9ef9facd8b7824a9c504dc0ba986f28ec4f60da2
DexterStorey <dexter@dexterstorey.com>
2025-04-07 10:49:38 -0400

    bump stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe'
 import env from './env'
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-	apiVersion: '2025-01-27.acacia',
+	apiVersion: '2025-02-24.acacia',
 	appInfo: {
 		name: 'Maige',
 		version: '0.1.0'
